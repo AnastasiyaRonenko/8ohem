5240803268f290327c9d3b559c4725b5f6685a56
AnastasiyaRonenko <ronenko94@gmail.com>
2018-03-12 21:09:29 +0200

    fifth commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	 $('.j-banner__sliders').slick({
+  		infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: false,
+  		autoplay: true,
+  		autoplaySpeed: 4000,
+  		dots: true
+	 });
+});
